d75a8f56eacaece1ee9f8a53ad180c919e3f005d
felix <felixvoit2014@gmail.com>
2026-06-02 09:12:00 +0200

    Improved Security for Calculator

--- Assets/Calculator.py
+++ Assets/Calculator.py
@@ -1,11 +1,19 @@
 import math
 
 def Calculator():
-    rechnung = input("> calculation: ")
+    rechnung = input("> calculation: ").replace(",",".")
 
     try:
+        for zeichen in rechnung:
+            if zeichen not in "0123456789+-*/().√sqrtpi":
+                raise ValueError
+
         result = eval(rechnung,{"__builtins__": None}, {"sqrt": math.sqrt, "pi": math.pi})
         print(result)
 
-    except:
-        print("❌ Invalid invoice")
+    except ZeroDivisionError:
+        print("❌ Division by zero")
+    except ValueError:
+        print("❌ Not a number")
+    except :
+        print("❌ Invalid input")
